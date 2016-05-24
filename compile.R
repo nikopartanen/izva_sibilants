ec0ae761182672a4efaeef0facff8c308de657d0
nikopartanen <nikotapiopartanen@gmail.com>
2016-05-24 15:33:32 +0200

    updated things

--- compile.R
+++ compile.R
@@ -1,2 +1,2 @@
-bookdown::render_book("index.rmd", "bookdown::gitbook", output_dir = "gh-pages")
+bookdown::render_book(input = "index.rmd", output_format = "bookdown::gitbook", output_dir = "gh-pages")
 # bookdown::render_book("index.rmd", "bookdown::pdf_book", output_dir = "_book")
