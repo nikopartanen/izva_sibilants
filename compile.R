9bcbaeffe963f287670e7d3f7423ee3077bdf3fb
Niko Partanen <nikotapiopartanen@gmail.com>
2017-04-13 15:58:26 +0200

    this builds both pdf and html

--- compile.R
+++ compile.R
@@ -1,2 +1,3 @@
+# devtools::install_github('rstudio/bookdown')
 bookdown::render_book(input = "index.rmd", output_format = "bookdown::gitbook", output_dir = "gh-pages")
-# bookdown::render_book("index.rmd", "bookdown::pdf_book", output_dir = "_book")
+bookdown::render_book("index.rmd", "bookdown::pdf_book")
